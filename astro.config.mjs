e7cf02fd0e3824c3bf80294b1cac2b5cef373c9c
Anto <bueno@uoc.edu>
2024-09-08 20:05:00 +0200

    Add @astrojs/netlify dependency to package.json and update astro.config.mjs to use the Netlify adapter instead of the Vercel adapter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel/serverless';
+// Import the Netlify adapter
+import netlify from '@astrojs/netlify/functions';  // Cambiamos a Netlify
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  output: 'server', // Puedes mantener esta línea si el proyecto requiere salida de servidor
+  adapter: netlify(), // Cambiamos a Netlify
 });
